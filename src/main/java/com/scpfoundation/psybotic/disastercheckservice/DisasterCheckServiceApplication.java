a7e8cdb7326749f687ea3129c62c737ee51fdd2f
beratyavas19 <i.beratyavas@gmail.com>
2021-03-01 23:46:58 +0300

    establish disaster check service

---
+++ src/main/java/com/scpfoundation/psybotic/disastercheckservice/DisasterCheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scpfoundation.psybotic.disastercheckservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterCheckServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisasterCheckServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scpfoundation/psybotic/disastercheckservice/DisasterCheckServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scpfoundation.psybotic.disastercheckservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisasterCheckServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
