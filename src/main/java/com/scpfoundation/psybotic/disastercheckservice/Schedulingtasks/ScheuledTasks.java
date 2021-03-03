0c41a32585df22a7400e7fcb43fedacd4259409d
beratyavas19 <i.beratyavas@gmail.com>
2021-03-04 02:38:21 +0300

    Adding Scheuled Task'

---
+++ src/main/java/com/scpfoundation/psybotic/disastercheckservice/Schedulingtasks/ScheuledTasks.java
@@ -0,0 +1,22 @@
+package com.scpfoundation.psybotic.disastercheckservice.Schedulingtasks;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheuledTasks {
+    private static final Logger log = LoggerFactory.getLogger(ScheuledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 3600000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+
+    }
+}
